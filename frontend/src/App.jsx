6930a62c1ef23e398b34fdc27b15b6e841fbc43f
Kassem Haidar <kassemhaidar551@gmail.com>
2025-04-17 15:05:54 +0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import SignUp from "./pages/signup/SignUp";
 
 const App = () => {
   const { authUser } = useAuthContext();
